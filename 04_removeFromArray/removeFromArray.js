387de4b7be3101bf601ebd0db179d37226d991c8
Jason N. Carvajal P <48056563+DPlilCoDeR@users.noreply.github.com>
2023-01-05 11:13:36 +0100

    04 removeFromArray 1 test pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (numArray, ...deleteNum) {
+    const index = numArray.indexOf(...deleteNum);
+    numArray.splice(index, deleteNum.length);
+    console.log(deleteNum.length);
+    return numArray;
 };
 
 // Do not edit below this line
