5907d3b372fb08578a64360cf511e97d2089c095
Jason N. Carvajal P <48056563+DPlilCoDeR@users.noreply.github.com>
2023-01-05 11:41:21 +0100

    05 sumAll 1 test pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (minor, bigger) {
+    let result = 0;
+    for (let i = minor; i <= bigger; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
