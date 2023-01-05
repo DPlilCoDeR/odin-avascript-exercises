b26207dba9e2d4904547314d2dd426bf8af27af5
Jason N. Carvajal P <48056563+DPlilCoDeR@users.noreply.github.com>
2023-01-05 13:43:35 +0100

    07 tempConversion ALL test pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+    // Formula
+    // (75F - 32) x 0.5556 = 23.6C
+    let converted = (temp - 32) * 0.5556;
+    return Math.round(converted * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+    //Formula
+    // (50C x 1.8) + 32 = 122F
+    let converted = temp * 1.8 + 32;
+    return Math.round(converted * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
