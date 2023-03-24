4d7f74409dbcf20f766aba256e706be864a5a382
Jason N. Carvajal P <48056563+DPlilCoDeR@users.noreply.github.com>
2023-03-24 08:05:35 +0100

    08_calculator all test pass

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+    const sum = array.reduce((partialSum, a) => partialSum + a, 0);
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+    return array.reduce((partial, a) => partial * a);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+    if (a > 0) {
+        return a * factorial(a - 1);
+    }
+    return 1;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
