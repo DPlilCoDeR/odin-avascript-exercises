ebad1757b38f547474d6b8bb5ba447f258462b1a
Jason N. Carvajal P <48056563+DPlilCoDeR@users.noreply.github.com>
2023-03-28 15:20:48 +0200

    09_palindromes all test pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const processed = string.toLowerCase().replace(/[^a-z]/g, "");
+    console.log(processed.split("").reverse().join(""), processed);
+    return processed.split("").reverse().join("") == processed;
 };
 
 // Do not edit below this line
