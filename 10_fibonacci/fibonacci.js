2e4e24370b48e30ac5a60bf6d0837d85487fe4e1
Jason N. Carvajal P <48056563+DPlilCoDeR@users.noreply.github.com>
2023-03-29 09:44:49 +0200

    10_fibonacci all test pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    if(number <= 0){
+        return "OOPS"
+    }
+    if (number == "1") {
+        return 1
+    }
+    let list = [0, 1];
+    for (let i = 2; i < number; i++) {
+        list[i] = list[i - 2] + list[i - 1];
+    }
+    return list[number - 1] + list[number - 2];
 };
 
 // Do not edit below this line
