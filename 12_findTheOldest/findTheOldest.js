9cfc7d5502a537d211b0f0d7b0f0ac2e1954f98a
Jason N. Carvajal P <48056563+DPlilCoDeR@users.noreply.github.com>
2023-04-03 18:34:05 +0200

    12_findTheOldest all test pass

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (lista) {
+    const objetoMayorEdad = lista.reduce((acumulado, objetoActual) => {
+        const duracionActual = objetoActual.yearOfDeath
+            ? objetoActual.yearOfDeath - objetoActual.yearOfBirth
+            : new Date().getFullYear() - objetoActual.yearOfBirth;
+        const duracionAcumulada = acumulado.yearOfDeath
+            ? acumulado.yearOfDeath - acumulado.yearOfBirth
+            : new Date().getFullYear() - acumulado.yearOfBirth;
+        return duracionActual > duracionAcumulada ? objetoActual : acumulado;
+    }, lista[0]);
+    return objetoMayorEdad;
 };
 
 // Do not edit below this line
